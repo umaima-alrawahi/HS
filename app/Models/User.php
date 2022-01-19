7809ad2b85ad5512bd9baec34fb170b619a1271b
umaima-alrawahi <omaima97alrawahi@gmail.com>
2022-01-20 02:08:54 +0400

    add profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Profile;
 
 class User extends Authenticatable
 {
@@ -47,4 +48,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            $profile = Profile::create(['user_id' => $user->id]);
+        });
+    }
 }
